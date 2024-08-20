728a5e00620511f1d3f05fbffbb54835a0db73dc
JosueGabriel0 <josue.ochoa@upeu.edu.pe>
2024-08-20 10:43:51 -0500

    Actualizacion 1.0

---
+++ ms-estudiante/src/main/java/upeu/edu/pe/msestudiante/MsEstudianteApplication.java
@@ -0,0 +1,13 @@
+package upeu.edu.pe.msestudiante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEstudianteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEstudianteApplication.class, args);
+	}
+
+}
