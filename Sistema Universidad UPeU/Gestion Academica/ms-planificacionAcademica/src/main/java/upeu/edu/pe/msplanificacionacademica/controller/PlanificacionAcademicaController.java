0f2596761746e6e7bc61dba3b23f92fce1e2c0c0
JosueGabriel0 <josue.ochoa@upeu.edu.pe>
2024-09-02 13:40:02 -0500

    a

--- Sistema Universidad UPeU/Gestion Academica/ms-planificacionAcademica/src/main/java/upeu/edu/pe/msplanificacionacademica/controller/PlanificacionAcademicaController.java
+++ Sistema Universidad UPeU/Gestion Academica/ms-planificacionAcademica/src/main/java/upeu/edu/pe/msplanificacionacademica/controller/PlanificacionAcademicaController.java
@@ -1,7 +1,43 @@
 package upeu.edu.pe.msplanificacionacademica.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import upeu.edu.pe.msplanificacionacademica.entity.PlanificacionAcademica;
+import upeu.edu.pe.msplanificacionacademica.service.PlanificacionAcademicaService;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping
 public class PlanificacionAcademicaController {
+    @Autowired
+    private PlanificacionAcademicaService planificacionAcademicaService;
+
+    @PostMapping
+    public ResponseEntity<PlanificacionAcademica> guardarPlanificacionAcademicaResponseEntity(@RequestBody PlanificacionAcademica planificacionAcademica){
+        return ResponseEntity.ok(planificacionAcademicaService.guardarPlanificacionAcademica(planificacionAcademica));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PlanificacionAcademica>> listarPlanificacionAcademicaResponseEntity() {
+        return ResponseEntity.ok(planificacionAcademicaService.listarPlanificacionAcademica());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PlanificacionAcademica> buscarPlanificacionAcademicaPorIdResponseEntity(@PathVariable(required = true) Long id){
+        return ResponseEntity.ok(planificacionAcademicaService.buscarPlanificacionAcademicaPorId(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PlanificacionAcademica> editarPlanificacionAcademicaResponseEntity(@PathVariable(required = true) Long id, @RequestBody PlanificacionAcademica planificacionAcademica){
+        planificacionAcademica.setIdPlanificacionAcademica(id);
+        return ResponseEntity.ok(planificacionAcademicaService.editarPlanificacionAcademica(planificacionAcademica));
+    }
+
+    @DeleteMapping("/{id}")
+    public String eliminarPlanificacionAcademicaResponseEntity(@PathVariable(required = true) Long id){
+        planificacionAcademicaService.eliminarPlanificacionAcademica(id);
+        return "Planificacion Academica Eliminado Correctamente";
+    }
 }
