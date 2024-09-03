fa1c7811fe9afd261622e32ec5f9bdc5585c94f8
JosueGabriel0 <josue.ochoa@upeu.edu.pe>
2024-09-02 22:02:28 -0500

    a

--- Sistema Universidad UPeU/Gestion Academica/ms-curso/src/main/java/upeu/edu/pe/mscurso/entity/Curso.java
+++ Sistema Universidad UPeU/Gestion Academica/ms-curso/src/main/java/upeu/edu/pe/mscurso/entity/Curso.java
@@ -1,10 +1,11 @@
 package upeu.edu.pe.mscurso.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -21,5 +22,24 @@ public class Curso {
     private int creditos;
     private String tipo;
     private String nivel;
+
+    @ElementCollection
+    @CollectionTable(name = "asignaturas", joinColumns = @JoinColumn(name = "curso_id"))
+    @Column(name = "asignatura")
+    private List<String> asignaturas = new ArrayList<>();
+
+    private LocalDateTime fechaCreacion;
+
+    private LocalDateTime fechaModificacion;
+
+    @PrePersist
+    public void onCreate(){
+        fechaCreacion = java.time.LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void onUpdate(){
+        fechaModificacion = java.time.LocalDateTime.now();
+    }
 
 }
---
+++ Sistema Universidad UPeU/Gestion Academica/ms-evaluacionAcademica/src/main/java/upeu/edu/pe/msevaluacionacademica/service/PersonaService.java
@@ -0,0 +1,18 @@
+package upeu.edu.pe.mspersona.service;
+
+import upeu.edu.pe.mspersona.entity.Persona;
+
+import java.util.List;
+
+public interface PersonaService {
+
+    public Persona guardarPersona(Persona persona);
+
+    public List<Persona> listarPersona();
+
+    public Persona buscarPersonaPorId(Long id);
+
+    public Persona editarPersona(Persona persona);
+
+    public void eliminarPersona(Long id);
+}
