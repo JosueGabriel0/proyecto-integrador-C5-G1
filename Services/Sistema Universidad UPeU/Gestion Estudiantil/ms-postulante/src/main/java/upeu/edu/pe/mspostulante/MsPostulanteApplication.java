2e74c1be522a9cb344c253278a4c246a52be4960
grimaldoupeu <grimaldoarredondomartinez@gmail.com>
2024-09-11 17:00:51 -0500

    a

---
+++ Services/Sistema Universidad UPeU/Gestion Estudiantil/ms-postulante/src/main/java/upeu/edu/pe/mspostulante/MsPostulanteApplication.java
@@ -0,0 +1,13 @@
+package upeu.edu.pe.mspostulante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPostulanteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsPostulanteApplication.class, args);
+    }
+
+}
