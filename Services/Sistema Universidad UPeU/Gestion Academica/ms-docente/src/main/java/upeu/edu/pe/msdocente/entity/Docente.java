80d046b996651819312ff58869550760db4481e6
JosueGabriel0 <josue.ochoa@upeu.edu.pe>
2024-09-15 10:29:54 -0500

    a

--- Services/Sistema Universidad UPeU/Gestion Academica/ms-docente/src/main/java/upeu/edu/pe/msdocente/entity/Docente.java
+++ Services/Sistema Universidad UPeU/Gestion Academica/ms-docente/src/main/java/upeu/edu/pe/msdocente/entity/Docente.java
@@ -22,7 +22,12 @@ public class Docente {
     private String tituloAcatemico;
     private String especialidad;
 
+    @ElementCollection
+    private List<String> cursosImpartidos;
 
+    // Historial Académico del Estudiante
+    @OneToMany(mappedBy = "docente", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<RegistroLaboral> historialLaboral;
 
     @Enumerated(EnumType.STRING)
     private EstadoLaboral estadoLaboral;
@@ -35,6 +40,26 @@ public class Docente {
     private String salario;
     private String horario;
 
+    @ElementCollection
+    @CollectionTable(name = "publicaciones_academicas", joinColumns = @JoinColumn(name = "docente_id"))
+    @Column(name = "publicacion")
+    private List<String> publicacionesAcademicas = new ArrayList<String>();
+
+    @ElementCollection
+    @CollectionTable(name = "proyectos_investigacion", joinColumns = @JoinColumn(name = "docente_id"))
+    @Column(name = "proyectos")
+    private List<String> proyectosInvestigacion = new ArrayList<String>();
+
+    private String numeroOficina;
+    private String extensionTelefonica;
+    private String supervisor;
+
+    @ElementCollection
+    @CollectionTable(name = "logros_academicos", joinColumns = @JoinColumn(name = "docente_id"))
+    @Column(name = "logros")
+    private List<String> logrosAcademicos = new ArrayList<String>();
+
+    private LocalDate fechaJubilacion;
     private long cursoId;
     @Transient
     private Curso curso;
