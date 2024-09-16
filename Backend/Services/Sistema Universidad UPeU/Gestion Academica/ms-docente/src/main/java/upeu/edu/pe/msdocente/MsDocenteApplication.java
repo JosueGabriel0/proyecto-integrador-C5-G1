5c81daeac70eca0febcb3d64aeb4356b52892d9d
JosueGabriel0 <josue.ochoa@upeu.edu.pe>
2024-09-15 23:09:28 -0500

    a

--- Backend/Services/Sistema Universidad UPeU/Gestion Academica/ms-docente/src/main/java/upeu/edu/pe/msdocente/MsDocenteApplication.java
+++ Backend/Services/Sistema Universidad UPeU/Gestion Academica/ms-docente/src/main/java/upeu/edu/pe/msdocente/MsDocenteApplication.java
@@ -3,10 +3,12 @@ package upeu.edu.pe.msdocente;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.modelmapper.ModelMapper;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -14,6 +16,11 @@ public class MsDocenteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsDocenteApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 	@Bean
