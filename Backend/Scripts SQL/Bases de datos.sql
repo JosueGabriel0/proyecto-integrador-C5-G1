ef6a7e94c5ebc84a1d55b673233455fa7ea664f1
JosueGabriel0 <josue.ochoa@upeu.edu.pe>
2024-09-17 16:53:14 -0500

    a

---
+++ Backend/Scripts SQL/Bases de datos.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE `ms-calendarioacademico`;
+CREATE DATABASE `ms-carrera`;
+CREATE DATABASE `ms-curso`;
+CREATE DATABASE `ms-docente`;
+CREATE DATABASE `ms-estudiante`;
+CREATE DATABASE `ms-evaluacionacademica`;
+CREATE DATABASE `ms-materialeseducativos`;
+CREATE DATABASE `ms-planificacionacademica`;
+CREATE DATABASE `ms-auth`;
+CREATE DATABASE `ms-persona`;
+CREATE DATABASE `ms-roles`;
+CREATE DATABASE `ms-usuarios`;
