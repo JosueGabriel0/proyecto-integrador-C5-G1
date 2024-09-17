0ab77aaaf242f4db8dde2b3f25e64039f01275f5
JosueGabriel0 <josue.ochoa@upeu.edu.pe>
2024-09-17 11:08:46 -0500

    a

--- Backend/Services/Sistema Universidad UPeU/Transversales/ms-auth/src/main/java/upeu/edu/pe/msauth/repository/AuthUserRepository.java
+++ Backend/Services/Sistema Universidad UPeU/Transversales/ms-auth/src/main/java/upeu/edu/pe/msauth/repository/AuthUserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-
+@Repository
 public interface AuthUserRepository extends JpaRepository<AuthUser,Integer> {
     Optional<AuthUser> findByUsername(String userName);
 }
