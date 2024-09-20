bf26f8c0fedce07b172ecc2df4cf9e5946b4bee5
JosueGabriel0 <josue.ochoa@upeu.edu.pe>
2024-09-19 23:15:41 -0500

    a

--- Backend/Services/Sistema Universidad UPeU/Transversales/ms-inscripciones/src/main/java/upeu/edu/pe/msinscripciones/controller/InscipcionesController.java
+++ Backend/Services/Sistema Universidad UPeU/Transversales/ms-inscripciones/src/main/java/upeu/edu/pe/msinscripciones/controller/InscipcionesController.java
@@ -15,42 +15,36 @@ public class InscipcionesController {
     @Autowired
     private InscripcionesService inscripcionesService;
 
-    // Método para crear una inscripción sin crear un rol (solo asignando el ID del rol)
     @PostMapping
     public ResponseEntity<Inscripcion> crearInscripcion(@RequestBody Inscripcion inscripcion) {
         Inscripcion nuevaInscripcion = inscripcionesService.crearInscripcion(inscripcion);
         return ResponseEntity.status(HttpStatus.CREATED).body(nuevaInscripcion);
     }
 
-    // Método para crear una inscripción y crear un rol
     @PostMapping("/con-rol")
     public ResponseEntity<Inscripcion> crearInscripcionConRol(@RequestBody Inscripcion inscripcion) {
         Inscripcion nuevaInscripcionConRol = inscripcionesService.crearInscripcionConRol(inscripcion);
         return ResponseEntity.status(HttpStatus.CREATED).body(nuevaInscripcionConRol);
     }
 
-    // Método para listar una inscripción por ID
     @GetMapping("/{id}")
     public ResponseEntity<Inscripcion> listarInscripcionPorId(@PathVariable Long id) {
         Inscripcion inscripcion = inscripcionesService.buscarInscripcionPorId(id);
         return ResponseEntity.ok(inscripcion);
     }
 
-    // Método para listar todas las inscripciones
     @GetMapping
     public ResponseEntity<List<Inscripcion>> listarInscripciones() {
         List<Inscripcion> inscripciones = inscripcionesService.listarInscripcion();
         return ResponseEntity.ok(inscripciones);
     }
 
-    // Método para editar una inscripción por ID
     @PutMapping("/{id}")
     public ResponseEntity<Inscripcion> editarInscripcion(@PathVariable Long id, @RequestBody Inscripcion nuevaInscripcion) {
         Inscripcion inscripcionEditada = inscripcionesService.editarInscripcion(id, nuevaInscripcion);
         return ResponseEntity.ok(inscripcionEditada);
     }
 
-    // Método para eliminar los datos de usuario, persona, estudiante o docente pero no el rol
     @DeleteMapping("/{id}")
     public ResponseEntity<Inscripcion> eliminarDatosInscripcion(@PathVariable Long id) {
         Inscripcion inscripcionActualizada = inscripcionesService.eliminarInscripcion(id);
