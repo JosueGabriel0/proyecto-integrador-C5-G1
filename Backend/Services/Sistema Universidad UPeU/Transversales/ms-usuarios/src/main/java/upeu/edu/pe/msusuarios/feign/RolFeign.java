9745535fcc144855f6aa43c59b95407fc62cb0b0
JosueGabriel0 <josue.ochoa@upeu.edu.pe>
2024-09-22 12:52:52 -0500

    a

--- Backend/Services/Sistema Universidad UPeU/Transversales/ms-usuarios/src/main/java/upeu/edu/pe/msusuarios/feign/RolFeign.java
+++ Backend/Services/Sistema Universidad UPeU/Transversales/ms-usuarios/src/main/java/upeu/edu/pe/msusuarios/feign/RolFeign.java
@@ -1,5 +1,6 @@
 package upeu.edu.pe.msusuarios.feign;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,5 +11,10 @@ import upeu.edu.pe.msusuarios.dto.Rol;
 public interface RolFeign {
 
     @GetMapping("/{id}")
+    @CircuitBreaker(name = "rolListarPorIdCB", fallbackMethod = "fallBackRolListarPorId")
     public ResponseEntity<Rol> listarRolDtoPorId(@PathVariable(required = true) long id);
+
+    default ResponseEntity<Rol> fallBackRolListarPorId(Long id, Exception e) {
+        return ResponseEntity.ok(new Rol());
+    }
 }
