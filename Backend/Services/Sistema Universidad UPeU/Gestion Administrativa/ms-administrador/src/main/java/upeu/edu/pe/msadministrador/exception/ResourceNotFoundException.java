6db368aa9821737ef93fbfc1111770eaccb859e1
JosueGabriel0 <josue.ochoa@upeu.edu.pe>
2024-09-29 19:00:44 -0500

    a

--- Backend/Services/Sistema Universidad UPeU/Gestion Administrativa/ms-administrador/src/main/java/upeu/edu/pe/msadministrador/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package upeu.edu.pe.msadministrador.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-
-    public ResourceNotFoundException(String message){
-        super(message);
-    }
-}
