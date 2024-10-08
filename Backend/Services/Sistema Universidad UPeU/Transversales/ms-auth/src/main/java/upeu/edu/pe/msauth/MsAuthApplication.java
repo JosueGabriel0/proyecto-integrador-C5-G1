e964753530511931727c315f4e369a8a3b281bd8
JosueGabriel0 <josue.ochoa@upeu.edu.pe>
2024-10-08 12:39:12 -0500

    a

--- Backend/Services/Sistema Universidad UPeU/Transversales/ms-auth/src/main/java/upeu/edu/pe/msauth/MsAuthApplication.java
+++ Backend/Services/Sistema Universidad UPeU/Transversales/ms-auth/src/main/java/upeu/edu/pe/msauth/MsAuthApplication.java
@@ -1,9 +1,13 @@
 package upeu.edu.pe.msauth;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -14,4 +18,14 @@ public class MsAuthApplication {
 		SpringApplication.run(MsAuthApplication.class, args);
 	}
 
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI().info(new Info()
+				.title("OPEN API MICROSERVICIO Auth")
+				.version("0.0.1")
+				.description("servicios web auth")
+				.termsOfService("http:// swagger.io/terms")
+				.license(new License().name("Apache 2.0").url("http://springdoc.org"))
+		);
+	}
 }
