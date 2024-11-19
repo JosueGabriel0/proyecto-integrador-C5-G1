ea969259bbcda25439e6f34653b0d267d68513cd
JosueGabriel0 <josue.ochoa@upeu.edu.pe>
2024-11-18 22:52:07 -0500

    a

--- Frontend/sistema-universidad-upeu/src/components/general/GeneralInicioComponent.jsx
+++ Frontend/sistema-universidad-upeu/src/components/general/GeneralInicioComponent.jsx
@@ -2,18 +2,11 @@ import { Link, useNavigate } from "react-router-dom";
 
 function GeneralInicioComponent() {
     const navigate = useNavigate();
-    const ADMINISTRADOR_BASE_REST_API_URL = `${process.env.REACT_APP_API_BASE_URL}/administrador`
 
-    function imprimir(){
-        return(
-            ADMINISTRADOR_BASE_REST_API_URL
-        )
-    }
     return (
         <div>
             <h1>Inicio</h1>
             <Link to="/login">LogIn</Link>
-            <h1>{imprimir()}</h1>
         </div>
     )
 }
