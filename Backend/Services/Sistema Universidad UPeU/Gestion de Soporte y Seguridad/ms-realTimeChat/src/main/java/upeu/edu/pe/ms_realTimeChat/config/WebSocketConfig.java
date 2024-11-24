1a4f8b03d3522ca7aa65c4900c6b03326faf8de7
JosueGabriel0 <josue.ochoa@upeu.edu.pe>
2024-11-23 20:15:42 -0500

    a

--- Backend/Services/Sistema Universidad UPeU/Gestion de Soporte y Seguridad/ms-realTimeChat/src/main/java/upeu/edu/pe/ms_realTimeChat/config/WebSocketConfig.java
+++ Backend/Services/Sistema Universidad UPeU/Gestion de Soporte y Seguridad/ms-realTimeChat/src/main/java/upeu/edu/pe/ms_realTimeChat/config/WebSocketConfig.java
@@ -13,7 +13,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("http://localhost:3000") // Permitir todos los orígenes
                 .withSockJS();
     }
 
